f1e37075b977c4c39e33309e0802ab68ade5c7d5
niw0906 <niw0906@naver.com>
2026-01-23 00:34:35 +0900

    feat: MQTT 메시지에서 payload까지 함께 로그하도록 개선

--- mqtt/mqtt_client.js
+++ mqtt/mqtt_client.js
@@ -12,6 +12,23 @@ function createMqttClient() {
 
   client.on("connect", () => {
     console.log("[MQTT] connected:", mqttUrl);
+
+    client.subscribe("feeder/#", { qos: 1 }, (err) => {
+      if (err) {
+        console.error("[MQTT] subscribe error:", err?.message ?? err);
+      } else {
+        console.log("[MQTT] subscribed: feeder/#");
+      }
+    });
+  });
+
+  client.on("message", (topic, payload) => {
+    const message = payload ? payload.toString("utf8") : "";
+    if (message.length > 0) {
+      console.log(`mqtt received:${topic}/${message}`);
+    } else {
+      console.log(`mqtt received:${topic}`);
+    }
   });
 
   client.on("error", (err) => {
