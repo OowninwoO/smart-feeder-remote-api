d6cc4a9ee4d8942c1580d461679db4b92305ef7c
niw0906 <niw0906@naver.com>
2026-03-02 22:02:49 +0900

    chore: 파이어베이스 인증 미들웨어 db.connect() 사용

--- middlewares/firebase_auth_middleware.js
+++ middlewares/firebase_auth_middleware.js
@@ -4,6 +4,8 @@ const db = require("../db");
 const firebaseAuthMiddleware = async (req, res, next) => {
   const token = req.headers.authorization.split(" ")[1];
 
+  const client = await db.connect();
+
   try {
     const decoded = await admin.auth().verifyIdToken(token);
 
@@ -21,7 +23,7 @@ const firebaseAuthMiddleware = async (req, res, next) => {
     req.provider = provider;
     req.uid = decoded.uid;
 
-    const selectResult = await db.query(
+    const selectResult = await client.query(
       `
       select id
       from users
@@ -37,7 +39,7 @@ const firebaseAuthMiddleware = async (req, res, next) => {
       return next();
     }
 
-    const insertResult = await db.query(
+    const insertResult = await client.query(
       `
       insert into users (provider, provider_user_id)
       values ($1, $2)
@@ -55,6 +57,8 @@ const firebaseAuthMiddleware = async (req, res, next) => {
       message: "유효하지 않은 토큰입니다.",
       data: null,
     });
+  } finally {
+    client.release();
   }
 };
 
